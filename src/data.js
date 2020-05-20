8463cbe4499bef3922384deeefa48657cdaa9899
Ricardo Sanchez <nardove@yahoo.com>
2020-05-20 22:08:47 +0100

    #

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const loadData = async (url) => {
 	return fetch(url)
 		.then((response) => response.json())
 		.then((data) => {
-			// console.log(data.results);
+			console.log(data.results);
 			return data;
 		})
 		.catch((error) => console.log(`There has been a problem: ${error}`));
